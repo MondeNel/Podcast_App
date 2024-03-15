a61a6e79eefe1149aca49363b0deb9d3786098f0
Monde Nel <Mondenel1996@gmail.com>
2024-03-15 18:12:12 +0200

    added functionality and imported the favorite icon and styled it with the functionality

--- podcast/src/Components/Header/CarouselCard.jsx
+++ podcast/src/Components/Header/CarouselCard.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import styles from './CarouselCard.module.css';
+import FavoriteIcon from '@mui/icons-material/Favorite';
 
-import image from '../../assets/faviconio-logo/image.jpg'; // Update the path to your image
+import image from '../../assets/faviconio-logo/image.jpg'; 
 
 const CarouselCard = () => {
   const settings = {
@@ -29,14 +30,32 @@ const CarouselCard = () => {
   // Array of placeholder text for cards
   const cardData = ['Card 1', 'Card 2', 'Card 3', 'Card 4', 'Card 5'];
 
+  const [iconColor, setIconColor] = useState('grey');
+
+  const handleIconClick = () => {
+    setIconColor(iconColor === 'grey' ? 'red' : 'grey');
+  };
+
   return (
     <div className={styles.carousel_container}>
       <h2>Trending Shows...</h2>
       <Slider {...settings}>
         {cardData.map((text, index) => (
           <div className={styles.card} key={index}>
+            <h3>Show name</h3>
             <img src={image} alt={text} className={styles.card_image} />
             <h3>{text}</h3>
+            <h4>Season</h4>
+            <p>Release date</p>
+            <p>Genre</p>
+            <div className={styles.bottom}>
+              <button className={styles.button}>View Details</button>
+              <FavoriteIcon
+                className={styles.icon}
+                style={{ color: iconColor }}
+                onClick={handleIconClick}
+              />
+            </div>
           </div>
         ))}
       </Slider>
