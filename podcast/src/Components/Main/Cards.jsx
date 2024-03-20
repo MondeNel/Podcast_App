d4253238044c1e8dcab86b51ad49334b40833ad0
Monde Nel <Mondenel1996@gmail.com>
2024-03-20 09:51:58 +0200

    created a 'show more' button that will function when the user clicks to see more content

--- podcast/src/Components/Main/Cards.jsx
+++ podcast/src/Components/Main/Cards.jsx
@@ -1,46 +1,55 @@
 import React, { useState } from 'react';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline';
+import Button from '@mui/material/Button';
 import styles from './Cards.module.css'; 
 
 import { showsData } from '../Data'; 
 
 function ShowCard({ show }) {
-
   const [iconColor, setIconColor] = useState('grey');
 
   const handleIconClick = () => {
     setIconColor(iconColor === 'grey' ? 'red' : 'grey');
   };
 
-    return (
-      <div className={styles.card}>
-        <img src={show.image} alt={show.title} />
-        <div className={styles.cardBody}>
-          <h3 className={styles.title}>{show.title}</h3>
-          <p className={styles.seasons}>Seasons: {show.seasons}</p>
-          <p className={styles.episodes}>Episodes: {show.episodes}</p>
-          <p className={styles.date}>Last Updated: {new Date(show.updated).toLocaleDateString()}</p>
-          <div className={styles.icons}>
-            <PlayCircleOutlineIcon className={styles.playButton} />
-            <FavoriteIcon
-                className={styles.favoriteIcon}
-                style={{ color: iconColor }}
-                onClick={handleIconClick}
-              />
-          </div>
+  return (
+    <div className={styles.card}>
+      <img src={show.image} alt={show.title} />
+      <div className={styles.cardBody}>
+        <h3 className={styles.title}>{show.title}</h3>
+        <p className={styles.seasons}>Seasons: {show.seasons}</p>
+        <p className={styles.episodes}>Episodes: {show.episodes}</p>
+        <p className={styles.date}>Last Updated: {new Date(show.updated).toLocaleDateString()}</p>
+        <div className={styles.icons}>
+          <PlayCircleOutlineIcon className={styles.playButton} />
+          <FavoriteIcon
+            className={styles.favoriteIcon}
+            style={{ color: iconColor }}
+            onClick={handleIconClick}
+          />
         </div>
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
 
 function ShowList() {
+  const [showAll, setShowAll] = useState(false);
+  const totalShows = showsData.length;
+  const initialLimit = 4;
+  const showLimit = showAll ? totalShows : initialLimit;
+
   return (
     <div className={styles.cardList}>
-      {showsData.map((show) => (
+      {showsData.slice(0, showLimit).map((show) => (
         <ShowCard key={show.id} show={show} />
       ))}
+      {!showAll && (
+        <Button onClick={() => setShowAll(true)} className={styles.showMoreButton}>
+          Show more
+        </Button>
+      )}
     </div>
   );
 }
