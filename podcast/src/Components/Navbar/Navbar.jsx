963ddea6550f73f00ef46caa4325b08ef96c6daf
Monde Nel <Mondenel1996@gmail.com>
2024-03-21 13:06:34 +0200

    styled the navbar

--- podcast/src/Components/Navbar/Navbar.jsx
+++ podcast/src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
-import styles from './Navbar.module.css'; // Import the CSS module
+import styles from './Navbar.module.css'; 
 import Search from './Search';
+import { Home, ContactMail, Info } from '@mui/icons-material';
 
 const Navbar = () => {
   return (
-    <div className={styles.topnav}> {/* Use the CSS class from the module */}
-      <a className={styles.active} href="#home">Podcast 3000</a> {/* Use the CSS class from the module */}
+    <div className={styles.topnav}> 
+      <a className={styles.active} href="#home">
+        <Home /> {/* Home icon */}
+        Home
+      </a>
+      <a href="#contact">
+        <ContactMail /> {/* Contact icon */}
+        Contact
+      </a>
+      <a href="#about">
+        <Info /> {/* About icon */}
+        About
+      </a>
       <Search />
     </div>
   );
