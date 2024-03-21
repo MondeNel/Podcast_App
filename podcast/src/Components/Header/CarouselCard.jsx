2295da419b3a373e0f9d294874f4234b47e9c5de
Monde Nel <Mondenel1996@gmail.com>
2024-03-21 10:07:03 +0200

    removed the favorite icon from the carousel 'trending shows'

--- podcast/src/Components/Header/CarouselCard.jsx
+++ podcast/src/Components/Header/CarouselCard.jsx
@@ -3,67 +3,68 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import styles from './CarouselCard.module.css';
-import FavoriteIcon from '@mui/icons-material/Favorite';
 import Dialog from '@mui/material/Dialog';
 import { DialogTitle, DialogContent, Typography, Select, MenuItem } from '@mui/material';
 import { PlayCircleOutline } from '@mui/icons-material';
 
 const showsData = [
-  {
-      id: "10716",
-      title: "Something Was Wrong",
-      description: "Something Was Wrong is an Iris Award-winning true-crime docuseries about the discovery, trauma, and recovery from shocking life events and abusive relationships.",
-      seasons: 14,
-      image: "https://content.production.cdn.art19.com/images/cc/e5/0a/08/cce50a08-d77d-490e-8c68-17725541b0ca/9dcebd4019d57b9551799479fa226e2a79026be5e2743c7aef19eac53532a29d66954da6e8dbdda8219b059a59c0abe6dba6049892b10dfb2f25ed90d6fe8d9a.jpeg",
-      genres: [1, 2],
-      updated: "2022-11-03T07:00:00.000Z"
-    },
+  
     {
-      id: "5675",
-      title: "This Is Actually Happening",
-      description: "What if your mother left to follow a cult… or you woke up in a morgue… or if your boat got caught in a storm and began to sink -- what would you do?   This is Actually Happening brings you extraordinary true stories of life-changing events told by the people who lived them. From a man who finds out a celebrity crush isn’t who she seems to a woman stranded in a Mexican desert fighting to survive, these stories will have you on the edge of your seat waiting to hear what happens next. New episodes come out every Tuesday for free. Listen 1-week early and to exclusive past episodes - all ad-free - with Wondery+ or Amazon Music with a Prime membership or Amazon Music Unlimited subscription.",
-      seasons: 12,
-      image: "https://content.production.cdn.art19.com/images/5a/4f/d4/19/5a4fd419-11af-4270-b31c-2c7ed2f563a5/bc913bc926be23d04c9a4d29a829269a753be3d2612dad91f7e92ba4618fefc5c8802af29a1d32b3261eb03f83613e2535e3c574469b0cb510c32cd8d94cfaa1.png",
-      genres: [2],
-      updated: "2022-11-01T10:00:00.000Z"
-    },
-    {
-      id: "5279",
-      title: "American History Tellers",
-      description: "The Cold War, Prohibition, the Gold Rush, the Space Race. Every part of your life - the words you speak, the ideas you share - can be traced to our history, but how well do you really know the stories that made America? We'll take you to the events, the times and the people that shaped our nation. And we'll show you how our history affected them, their families and affects you today. Hosted by Lindsay Graham (not the Senator). From Wondery, the network behind American Scandal, Tides of History, American Innovations and more. New episodes come out every Wednesday for free, with 1-week early access for Wondery+ subscribers. Listen ad-free on Wondery+ or on Amazon Music with a Prime membership or Amazon Music Unlimited subscription.",
-      seasons: 51,
-      image: "https://content.production.cdn.art19.com/images/a4/8f/53/79/a48f5379-a90e-4197-915c-c0645e0d9336/8d9e6ebc4d65a9575fa626318e426fcf8be7f98ea0c1b7b103de2b32def46ded57537627d80b36f55edf7c9a8ad639bd9816f68c79b56845203a0b5bc4a63a55.png",
-      genres: [3],
-      updated: "2022-11-02T07:01:00.000Z"
-    },
-    {
-      id: "10539",
-      title: "Scamfluencers",
-      description: "You never really know someone…especially online. In today’s world, the power of influence can be the quickest path to money and fame, and it often ends in ruin. These are the stories of the world’s most insidious Scamfluencers. And we are their prey. On Wondery’s new weekly series, join co-hosts Scaachi Koul and Sarah Hagi as they unpack epic stories of deception from the worlds of social media, fashion, finance, health, and wellness. These influencers claim to be everything from charismatic healers to trusted financial insiders to experts in dating. They cast spells over millions. Why do we believe them, and how does our culture allow them to thrive? From Black Swan Murder to a fake social media influencer to an audacious Hollywood Ponzi schemer, each season will take the listener along the twists and turns, the impact on victims, and what’s left when the facade falls away. New episodes come out every Monday for free, with 1-week early access for Wondery+ subscribers. Listen ad-free on Wondery+ or on Amazon Music with a Prime membership or Amazon Music Unlimited subscription.",
-      seasons: 3,
-      image: "https://content.production.cdn.art19.com/images/19/f4/f9/af/19f4f9af-4a18-44e1-a622-726f43feb79d/539a50f79529628dbde7aa116778056619b802bfa0247cb739db907085e0b595a5521efc78faa831ebddc235d69beb27e1e36fd51f825bc888f0c11cccbd9cd8.png",
-      genres: [2],
-      updated: "2022-10-24T07:01:00.000Z"
-    },
-    {
-      id: "9177",
-      title: "Killer Psyche",
-      description: "When a shocking crime occurs, people ask “Why?” Was it about power, ego, or revenge? On Killer Psyche, retired FBI agent Candice DeLong draws on her decades of experience to reveal why these murderers and criminals committed these acts. She will reveal fascinating new details about what drove these people, including cases she was close to. Candice will share specific psychological methods and profiling techniques that experts use to understand the deepest part of the most complex, fascinating, and twisted minds, from the Unabomber to Dr. Death to El Chapo. Listen to new episodes 1-week early and to all episodes ad-free with Wondery+ or on Amazon Music with a Prime membership or Amazon Music Unlimited subscription. Join Wondery+ here: https://wondery.app.link/killerpsyche",
+      id: "9705",
+      title: "Becoming Mother Nature",
+      description: "Becoming Mother Nature introduced us to troubled 13-year-old Chloe Lovejoy, who prematurely inherited the mantle and awesome power of Mother Nature from her Grandmother Ivy. In season one, Chloe struggled mightily to balance the trials and tribulations of middle school with...you know, saving the world. With Cupid and the Reaper, Volume II of “The Natureverse,” Chloe will soon learn she’s not the only super-powered teenager in the world. Meet thirteen-year-old Mondo Ramirez, a hopeless romantic in Savannah, Georgia, who rarely thinks of consequences. When his elaborate 8th grade promposal goes awry, Mondo accidentally kills the Grim Reaper, inheriting the Reaper’s cloak and scythe in the process. Marcus Aronson never accepted or understood his parents’ divorce. Logical and socially challenged, Marcus went on a mission to find Cupid himself, and finally convinced the winged archer him to hand over his arrows and his title. Armed with research, algorithms and a huge pair of wings, Marcus is determined to change the way people fall in love. Cupid and The Reaper, Volume II of the “Natureverse,” tells the buddy-adventure story of these two mismatched and unlikely heroes. Can Marcus and Mondo learn to work together and harness the powers of life, death and love to fight off a mysterious supernatural force in their school? And will Chloe see them as friends...or foes? For more great Gen-Z Media podcasts visit: http://gzmshows.com",
       seasons: 2,
-      image: "https://content.production.cdn.art19.com/images/68/4e/03/af/684e03af-29c5-4a35-b84a-d929f114caee/4f60eec3fabd62251d0cdbd1af11b79c43fb1465dbc5ec3371328fbddadee597e9f115c31b079e20266648ee07a80a93c01cecdb81ab3545d872393997594ef3.png",
-      genres: [3, 2],
-      updated: "2022-11-01T07:03:00.000Z"
+      image: "https://content.production.cdn.art19.com/images/82/13/f6/9a/8213f69a-882f-49ae-b25b-aa0d91c27fe3/f2c8dcc41fe54ea9d738b6a73942a1ab3be4d2c6e528e7b24121cf89275a7944aab19b3f1ae44e5c65039510a17903e42577e0fb977ab8af2e388d0b3273c476.jpeg",
+      genres: [9],
+      updated: "2021-10-14T09:00:00.000Z"
     },
     {
-      id: "6807",
-      title: "Even the Rich",
-      description: "Our lives can be crazy, but you can take a break from it all with Wondery’s new series, Even the Rich, where co-hosts Brooke Siffrinn and Aricia Skidmore-Williams pull back the curtain and chat about someone else’s craziness for a change. They tell stories about some of the greatest family dynasties in history, from the Murdochs to the Royals to the Carters (Jay-Z and Beyoncé, that is). Because as Queen Elizabeth once said, “A good gossip is a wonderful tonic.” New episodes come out every Tuesday for free. Wondery+ subscribers get early access to new full seasons and all episodes ad-free.",
-      seasons: 33,
-      image: "https://content.production.cdn.art19.com/images/c3/55/d2/da/c355d2da-f845-47df-a4e6-22b70a5082bb/c290fe89d3a699dd5c316f5f4cfe2ca942183cef5d6ac4fc2d7d6df296690c9e7183f79422dcb0b37af7c7e7e59de0e36cddd3b01500bf066a470614c9a0af6d.png",
-      genres: [4, 5, 3],
-      updated: "2022-11-01T07:08:00.000Z"
+      id: "10934",
+      title: "WeWow",
+      description: "Hosted by Mindy Thomas and Guy Raz of  Wow in the World, WeWow  is a project-based learning podcast with activities curious kids and grownups can bring to life in their world! Built around themes like Earth Day, Back-to-school, Black History Month and Summer Camp,  WeWow  invites kids to tinker like real scientists by trying experiments or activities at home and sharing their findings through photos, videos, or audio recordings.",
+      seasons: 2,
+      image: "https://content.production.cdn.art19.com/images/e0/ba/41/4d/e0ba414d-7805-4435-bcdb-87ef5ccab9a1/25d3351125c43b70beff87de28397fbcf5ea1a7d86d8d2b14ebd0226c8c6ffdb7a723df7a86475d6165c391f4b838fbcc4b6da05f380dc94af40f2b168e99945.jpeg",
+      genres: [9],
+      updated: "2022-10-27T07:01:00.000Z"
     },
-];
+    {
+      id: "9694",
+      title: "Young Ben Franklin",
+      description: "Before he was Benjamin Franklin, inventor and statesman, he was just Ben, a boy in Colonial Boston with an adventurous spirit, a curious mind...and a penchant for getting into trouble. Meet our most endearing founding father at fourteen; a charming rebel years away from discovering the ageless sayings and brilliant inventions that made him famous. When Ben and his friends stumble upon a mysterious letter leading to a legendary treasure, he'll have to use his wits and bravery to outsmart the cruel British governor of Massachusetts. For more great Gen-Z podcasts visit http://gzmshows.com",
+      seasons: 2,
+      image: "https://content.production.cdn.art19.com/images/27/5c/b2/90/275cb290-4a0c-40e1-9015-a944ef3cdaa0/d8a37376115fa95a213434d69a98558cd0eb8c9bc2b24ae75157f6058464ed42c0c5b2ea099e6acbe89e28e20482a9789bff6333d338a8dd1b70cadc7e378e2d.jpeg",
+      genres: [9],
+      updated: "2022-04-13T07:01:00.000Z"
+    },
+    {
+      id: "9263",
+      title: "True Love",
+      description: "Love is that first breathless glance. That person you lust after. That person you fall too hard for.   But “happily ever after” ain’t ever in the cards. In fact, it’s more like, “ Hurry up before my husband gets home!”  And that is True Love. From Wondery, True Love is a weekly fictional series of scandalous love stories that are sexy, funny and filled to the brim with heartache. It’s the marriage of your favorite nighttime soap opera with your favorite juicy beach read. True Love explores the obstacles and challenges of walking the path to happily ever after...or happy for the moment. Co-hosted by Amber Reauchean Williams and Malcolm Barrett.",
+      seasons: 6,
+      image: "https://content.production.cdn.art19.com/images/d4/c3/24/af/d4c324af-61b8-4905-816e-d33bd7cb3eba/2a63f7fa4a654c8582a18e490838cfb10facccf8464bd2b588bdb13079e82b38542f12e9a6f579cb545f5c7418de411f90f918a4b956b63e9ec0a620e2e37891.png",
+      genres: [7],
+      updated: "2022-06-29T07:30:00.000Z"
+    },
+    {
+      id: "8760",
+      title: "Secret Sauce",
+      description: "How do certain companies succeed where others have failed? What’s their “secret sauce”...and how can we get our hands on their recipes? From Wondery, the makers of the hit podcasts Business Wars and WeCrashed, comes SECRET SAUCE. Co-hosts Samuel Donner and John Frye take you behind the scenes of the most innovative leaders, trailblazing companies, and thriving cultures to learn what they have done to become a success. From how design guru Jony Ive worked with Steve Jobs to create some of Apple’s most iconic products, to how Warren Buffett invests better than just about anyone else. These are the unique recipes for what it takes to get to the top and stay there. Available ad-free on Wondery+ or on Amazon Music with a Prime membership or Amazon Music Unlimited subscription.",
+      seasons: 4,
+      image: "https://content.production.cdn.art19.com/images/db/ea/78/3d/dbea783d-82ee-430f-8db9-cb8409e3ae22/e58264bc3e60343216283d8ce23c35b4c077ac8e486b2fec393f121b24104d1b862b7084b11e2fa3ffea6404b668d5b03ddf4254552cb8d79af0ab5dff3e8e5b.png",
+      genres: [6, 3],
+      updated: "2021-07-13T09:00:00.000Z"
+    },
+    {
+      id: "9693",
+      title: "Treasure Island 2020",
+      description: "James Hawkins helps his mom run a motel in modern day Montauk, Long Island. But when a mysterious man washes up on the beach with a treasure map tattooed on his chest, James discovers that Billy Bones is, in fact, a time traveling pirate from the 18th century. James and his new friends, Morgan and Max, follow the map right into a magical portal that leads them back nearly three hundred years and back into a swashbuckling adventure. For more great Gen-Z podcasts visit http://gzmshows.com Become a Gen-Z Superfan by visiting our Patreon page for early access to shows, scripts, and other exclusive content. http://patreon.com/gzmshows",
+      seasons: 1,
+      image: "https://content.production.cdn.art19.com/images/2f/59/1c/9d/2f591c9d-76f3-44de-9e18-e08357760a1b/7981ea59f91389ae43cd403bca43acfebf923edd4b8be32f1b668a6bc587297a9524c48d5f9a924490dd6558c90bf07ee9f790e6811ad77dd112482e0d19d347.jpeg",
+      genres: [9],
+      updated: "2020-06-15T09:00:00.000Z"
+    },
+]
+
 
 const CarouselCard = () => {
   const settings = {
@@ -182,16 +183,11 @@ const CarouselCard = () => {
               <p className={styles.genres}>
                 {show.genres.map((genreId) => {
                   const genreName = genreMapping[genreId];
-                  return genreName.length > 10 ? genreName.slice(0, 10) + '...' : genreName;
+                  return genreName.length > 12 ? genreName.slice(0, 12) + '...' : genreName;
                 })}
               </p>
               <div className={styles.bottom}>
                 <button className={styles.button} onClick={() => handleViewShow(show)}>View show</button>
-                <FavoriteIcon
-                  className={styles.icon}
-                  style={{ color: iconColor }}
-                  onClick={handleIconClick}
-                />
               </div>
             </div>
           ))}
